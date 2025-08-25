ff71c3b3bb8fa24f369058fbb96f8eaa67420dc9
Rute-nunes <ruteaggretsuko@gmail.com>
2025-08-25 08:50:28 -0300

    Update Principal.java

--- Lista-01/Exercicios-Resolvidos/Q01/src/br/edu/principal/Principal.java
+++ Lista-01/Exercicios-Resolvidos/Q01/src/br/edu/principal/Principal.java
@@ -1,17 +1,25 @@
 package br.edu.principal;
-
+import.java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
 		int  n1, n2 , n3 ,n4 ,result;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("digite n1:");
+		n1=sc.nextInt();
+		System.out.println("digite n2:");
+		n2=sc.nextInt();
+		System.out.println("digite n3:");
+		n3=sc.nextInt();
+		System.out.println("digite n4:");
+		n4=sc.nextInt();
 		
-		n1=1;
-		n2=2;
-		n3=3;
-		n4=4;
 		result=n1+n2+n3+n4;
+		
+		
  
 	System.out.print(result);
 	}
 
 }
+
