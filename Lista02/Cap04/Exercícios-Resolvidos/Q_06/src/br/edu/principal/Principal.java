b33efb1a180a23c930f61aa607435fea2daed93b
Rute-Nunes <ruteaggretsuko@gmail.com>
2025-09-24 09:02:41 -0300

    Add files via upload

---
+++ Lista02/Cap04/Exercícios-Resolvidos/Q_06/src/br/edu/principal/Principal.java
@@ -0,0 +1,31 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int num,r;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Digite um número:");
+		num=sc.nextInt();
+		
+		r=num%2;
+		
+		if (r == 0) {
+			System.out.println("número par");
+			
+		}
+		else {
+			System.out.println("número impar");
+		}
+			
+		
+		
+		
+		
+		
+	}
+
+}
